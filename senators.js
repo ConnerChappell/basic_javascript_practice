225cf94cc265807ec5347d02dc0b22631f482dec
Conner Chappell <connerchappell.media@gmail.com>
2019-11-12 12:43:19 -0700

    added some filter, map, and reduce

--- senators.js
+++ senators.js
@@ -10,10 +10,51 @@ async function getAPIData(url) {
 
 // now, use the returned async data
 let allSenators = []
+let simpleSenators = []
+let republicans = []
+let democrats = []
+
 const theData = getAPIData('senators.json').then(data => {
     allSenators = data.results[0].members
-    populateDOM(allSenators)
+    simpleSenators = makeSimpleMap(allSenators)
+    republicans = filterSenators(simpleSenators, "R")
+    democrats = filterSenators(simpleSenators, "D")
+    populateDOM(simpleSenators)
 })
+
+// map example
+function makeSimpleMap(allOfThem) {
+    let results = allOfThem.map(senator => {
+        return {
+            id: senator.id,
+            name: `${senator.first_name} ${senator.last_name}`,
+            party: senator.party,
+            age: `${calculate_age(new Date(senator.date_of_birth))}`,
+            gender: senator.gender,
+            total_votes: senator.total_votes,
+        }
+    })
+    return results
+}
+
+// filter example
+function filterSenators(simpleList, partyAffiliation) {
+    return simpleList.filter(senator => senator.party === partyAffiliation)
+}
+
+// reduce example
+const testArray = [5,10,15,20,25,30,35,40,45,50,30]
+
+const testReduce = testArray.reduce((acc, num) => {
+    return acc + num
+}, 0)
+
+function totalVotes(senatorList) {
+    const results = senatorList.reduce((acc, senator) => {
+        return acc + senator.total_votes
+    }, 0)
+    return results
+}
 
 const container = document.querySelector('.container')
 
@@ -45,18 +86,34 @@ function cardContent(senator) {
     let mediaLeft = document.createElement('div')
     mediaLeft.setAttribute('class', 'media-left')
     let figure = document.createElement('figure')
-    figure.setAttribute('class', 'image is-48x48')
+    figure.setAttribute('class', 'image is-96x96')
     let img = document.createElement('img')
-    img.src = `https://bulma.io/images/placeholders/96x96.png`
+    if(senator.party === "R") {
+        img.src = `images/republican.png`
+    }
+    if(senator.party === "D") {
+        img.src = `images/democrat.png`
+    }
+    if(senator.party === "ID") {
+        img.src = `ttps://bulma.io/images/placeholders/96x96.png`
+    }
     img.alt = 'Placeholder image'
     let mediaContent = document.createElement('div')
     mediaContent.setAttribute('class', 'media-content')
     let titleP = document.createElement('p')
-    titleP.setAttribute('class', 'title is-4')
-    titleP.textContent = `${senator.first_name} ${senator.last_name}`
+    titleP.setAttribute('class', 'title is-5')
+    titleP.textContent = senator.name
     let subtitleP = document.createElement('p')
     subtitleP.setAttribute('class', 'subtitle is-6')
-    subtitleP.textContent = `${senator.state_rank}`
+    // subtitleP.textContent = `foo`
+
+    let contentDiv = document.createElement('div')
+    contentDiv.setAttribute('class', 'content')
+    contentDiv.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Phasellus nec iaculis mauris.`
+    let contentBreak = document.createElement('br')
+    let ageP = document.createElement('p')
+    ageP.textContent = senator.age
 
     mediaContent.appendChild(titleP)
     mediaContent.appendChild(subtitleP)
@@ -64,6 +121,17 @@ function cardContent(senator) {
     mediaLeft.appendChild(figure)
     media.appendChild(mediaLeft)
     media.appendChild(mediaContent)
+
+    contentDiv.appendChild(contentBreak)
+    contentDiv.appendChild(ageP)
     cardContent.appendChild(media)
+    cardContent.appendChild(contentDiv)
     return cardContent
+}
+
+function calculate_age(dob) { 
+    let diff_ms = Date.now() - dob.getTime();
+    let age_dt = new Date(diff_ms); 
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
